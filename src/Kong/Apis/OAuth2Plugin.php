1580d9e4b374f22e78bb989784b69df0a59e2c92
samhassell <sam@webzen.com.au>
2017-03-01 15:50:21 +0800

    Add oauth2 endpoint

--- src/Kong/Apis/OAuth2Plugin.php
+++ src/Kong/Apis/OAuth2Plugin.php
@@ -18,4 +18,18 @@ class OAuth2Plugin extends AbstractApi
       $body   = $this->createRequestBody($options);
       return $this->call('post', 'oauth2_tokens', [], $body);
   }
+  
+  /**
+   * Get a specific client application.
+   *
+   * @see https://getkong.org/plugins/oauth2-authentication/#the-flow-explained
+   *
+   * @param string $clientId
+   * @param array $params
+   * @return \stdClass
+   */
+  public function oauth2($clientId, array $params = [])
+  {
+      return $this->call('get', 'oauth2?client_id=' . $clientId, $params);
+  }
 }
